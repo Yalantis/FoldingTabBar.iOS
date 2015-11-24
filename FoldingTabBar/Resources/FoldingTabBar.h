aac7599e245e49aa29fb89923441e80c4984d6ca
Serhii Butenko <sereja.butenko@gmail.com>
2015-11-24 14:07:35 +0200

    Prepare shared schema

---
+++ FoldingTabBar/Resources/FoldingTabBar.h
@@ -0,0 +1,9 @@
+// For License please refer to LICENSE file in the root of YALAnimatingTabBarController project
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoldingTabBar.
+FOUNDATION_EXPORT double FoldingTabBarVersionNumber;
+
+//! Project version string for FoldingTabBar.
+FOUNDATION_EXPORT const unsigned char FoldingTabBarVersionString[];
