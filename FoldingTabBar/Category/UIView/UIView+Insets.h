128d10a0fd79a10979182e77ff8ff157fd5c8c2a
Sergey <sergey@Mac-mini-Jack.local>
2017-11-22 18:10:41 +0200

    Add supporting of iPhone x

---
+++ FoldingTabBar/Category/UIView/UIView+Insets.h
@@ -0,0 +1,15 @@
+//
+//  UIView+Insets.h
+//  FoldingTabBar
+//
+//  Created by Sergey on 11/22/17.
+//  Copyright © 2017 Sergey Butenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Insets)
+
+@property(nonatomic, assign, readonly) CGFloat bottomInset;
+
+@end
