20b52269210e8ec69f4672c06777dc51362fce10
Luke Switzer <luke@rootdowndigital.com>
2018-12-27 08:28:57 -0500

    Update UIView+Insets.m

--- FoldingTabBar/Category/UIView/UIView+Insets.m
+++ FoldingTabBar/Category/UIView/UIView+Insets.m
@@ -13,6 +13,12 @@
 #define IPHONE_X_PORTRAIT_SIZE CGSizeMake(1125, 2436)
 #define IPHONE_X_LANDSCAPE_SIZE CGSizeMake(2436, 1125)
 
+#define IPHONE_XR_PORTRAIT_SIZE CGSizeMake(828, 1792)
+#define IPHONE_XR_LANDSCAPE_SIZE CGSizeMake(1792, 828)
+
+#define IPHONE_XSM_PORTRAIT_SIZE CGSizeMake(1242 , 2688)
+#define IPHONE_XSM_LANDSCAPE_SIZE CGSizeMake(2688, 1242)
+
 #define IS_IPHONE [[UIDevice currentDevice]userInterfaceIdiom]==UIUserInterfaceIdiomPhone
 
 BOOL isIPhoneX() {
@@ -20,7 +26,13 @@ BOOL isIPhoneX() {
         BOOL isLandscapeIPhoneX = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_X_LANDSCAPE_SIZE);
         BOOL isPortraitIPhoneX = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_X_PORTRAIT_SIZE);
         
-        return isLandscapeIPhoneX || isPortraitIPhoneX;
+        BOOL isLandscapeIPhoneXS = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XR_LANDSCAPE_SIZE);
+        BOOL isPortraitIPhoneXS = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XR_PORTRAIT_SIZE);
+        
+        BOOL isLandscapeIPhoneXSM = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XSM_LANDSCAPE_SIZE);
+        BOOL isPortraitIPhoneXSM = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XSM_PORTRAIT_SIZE);
+        
+        return isLandscapeIPhoneX || isPortraitIPhoneX || isPortraitIPhoneXS || isLandscapeIPhoneXS || isPortraitIPhoneXSM || isLandscapeIPhoneXSM;
     }
     return false;
 }
