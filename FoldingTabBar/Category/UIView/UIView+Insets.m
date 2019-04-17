7a01ebd0b7315b96bab35569415cd0fb5163836d
Luke Switzer <luke@rootdowndigital.com>
2019-04-17 11:20:50 -0600

    add iOS check

--- FoldingTabBar/Category/UIView/UIView+Insets.m
+++ FoldingTabBar/Category/UIView/UIView+Insets.m
@@ -16,24 +16,31 @@
 #define IPHONE_XR_PORTRAIT_SIZE CGSizeMake(828, 1792)
 #define IPHONE_XR_LANDSCAPE_SIZE CGSizeMake(1792, 828)
 
-#define IPHONE_XSM_PORTRAIT_SIZE CGSizeMake(1242 , 2688)
-#define IPHONE_XSM_LANDSCAPE_SIZE CGSizeMake(2688, 1242)
+#define IPHONE_XS_PORTRAIT_SIZE CGSizeMake(1242, 2688)
+#define IPHONE_XS_LANDSCAPE_SIZE CGSizeMake(2688, 1242)
 
 #define IS_IPHONE [[UIDevice currentDevice]userInterfaceIdiom]==UIUserInterfaceIdiomPhone
 
 BOOL isIPhoneX() {
     if (IS_IPHONE) {
-        BOOL isLandscapeIPhoneX = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_X_LANDSCAPE_SIZE);
-        BOOL isPortraitIPhoneX = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_X_PORTRAIT_SIZE);
-        
-        BOOL isLandscapeIPhoneXS = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XR_LANDSCAPE_SIZE);
-        BOOL isPortraitIPhoneXS = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XR_PORTRAIT_SIZE);
-        
-        BOOL isLandscapeIPhoneXSM = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XSM_LANDSCAPE_SIZE);
-        BOOL isPortraitIPhoneXSM = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XSM_PORTRAIT_SIZE);
-        
-        return isLandscapeIPhoneX || isPortraitIPhoneX || isPortraitIPhoneXS || isLandscapeIPhoneXS || isPortraitIPhoneXSM || isLandscapeIPhoneXSM;
+        if (@available(iOS 8.0, *)) {
+            BOOL isLandscapeIPhoneX = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_X_LANDSCAPE_SIZE);
+            BOOL isPortraitIPhoneX = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_X_PORTRAIT_SIZE);
+
+            BOOL isLandscapeIPhoneXR = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XR_LANDSCAPE_SIZE);
+            BOOL isPortraitIPhoneXR = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XR_PORTRAIT_SIZE);
+
+            BOOL isLandscapeIPhoneXS = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XS_LANDSCAPE_SIZE);
+            BOOL isPortraitIPhoneXS = CGSizeEqualToSize(NATIVE_SIZE, IPHONE_XS_PORTRAIT_SIZE);
+
+            //Return true for any iPhone X and above models
+            return isLandscapeIPhoneX || isPortraitIPhoneX || isPortraitIPhoneXR || isLandscapeIPhoneXR
+            || isLandscapeIPhoneXS || isPortraitIPhoneXS;
+        } else {
+            // Fallback on earlier versions
+        }
     }
+    
     return false;
 }
 
@@ -43,7 +50,7 @@ BOOL isIPhoneX() {
     if (@available(iOS 11, *)) {
         if (isIPhoneX()) {
             //Default bottom inset for iPhone X at portrait orientation
-            return 34.f;
+            return 14.f;
         }
     }
     return 0.f;
